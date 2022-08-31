7716e000a42986e59106e811487cf9a212b87566
KaueSenac <kauegomesfenerick@gmail.com>
2022-08-31 20:00:04 -0300

    Primeiro commit

---
+++ inc/menu.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">SenaGran</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">
+                    <i class="bi bi-house-door-fill"></i>
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="postagem.php">
+                    <i class="bi bi-chat-dots-fill"></i>
+                        Postagens
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="usuarios.php">
+                    <i class="bi bi-people-fill"></i>
+                        Usuários
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sair.php">
+                    <i class="bi bi-box-arrow-right"></i>
+                        Sair
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ inc/rodape.php
@@ -0,0 +1,3 @@
+<div>
+    <p> Todos os direitos reservados</p>
+</div>
